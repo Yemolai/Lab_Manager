e96740ea8edc139333d9580e2d4a8d9f356fe898
Diego Garcia Cordeiro Souza <diego00alfa@gmail.com>
2019-04-18 14:47:46 -0300

    Setup of basic routes and layouts.

--- LabManager.py
+++ LabManager.py
@@ -5,7 +5,35 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('main.html')
+    return render_template("home.html")
+
+@app.route("/frequency")
+def frequency():
+    return render_template("frequency.html", title="Personel Frequency")
+
+@app.route("/lendings")
+def lendings():
+    return render_template("lendings.html", title="Equipment Lendings")
+
+@app.route("/technical")
+def technical():
+    return render_template("technical.html", title="Technical Issues")
+
+@app.route("/notices")
+def notices():
+    return render_template("notices.html", title="Notice Board")
+
+@app.route("/news")
+def news():
+    return render_template("news.html", title="News")
+
+@app.route("/calendar")
+def calendar():
+    return render_template("calendar.html", title="Calendar")
+
+@app.route("/about")
+def about():
+    return render_template("about.html", title="About")
 
 if __name__ == '__main__':
     app.run(debug=True)
