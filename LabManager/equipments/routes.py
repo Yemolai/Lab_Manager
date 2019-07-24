c4f86a484d7f3cef6d07134e2cafe0461c09878d
Diego Garcia Cordeiro Souza <diego00alfa@gmail.com>
2019-07-24 01:56:44 -0300

    Added basic CRUD operations endpoints to the equipments module.

--- LabManager/equipments/routes.py
+++ LabManager/equipments/routes.py
@@ -1,5 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, request, jsonify, render_template
 from flask_login import login_required
+from LabManager import db
+from LabManager.dbModels import Inventory, Lendings, TechnicalIssues
+from LabManager.maSchemas import equipment_schema, equipments_schema
 
 
 equipments = Blueprint("equips", __name__)
@@ -9,6 +12,54 @@ equipments = Blueprint("equips", __name__)
 @login_required
 def inventory():
     return render_template("inventory.html", title="Equipment Inventory")
+
+
+@equipments.route("/inventory/all", methods=["GET"])
+def inventory_all():
+    inventory = Inventory.query.all()
+    result = equipments_schema.dump(inventory)
+
+    return jsonify(result.data)
+
+
+@equipments.route("/inventory/<id>", methods=["GET"])
+def inventory_fetch(id):
+    equipment = Inventory.query.get(id)
+    result = equipment_schema.dump(equipment)
+
+    return jsonify(result.data)
+
+
+@equipments.route("/inventory/add", methods=["POST"])
+def inventory_add():
+    name = request.json["name"]
+    description = request.json["description"]
+    new_equip = Inventory(name=name, description=description)
+    db.session.add(new_equip)
+    db.session.commit()
+
+    return equipment_schema.jsonify(new_equip)
+
+
+@equipments.route("/inventory/update/<id>", methods=["PUT"])
+def inventory_put(id):
+    equipment = Inventory.query.get(id)
+    name = request.json["name"]
+    description = request.json["description"]
+    equipment.name = name
+    equipment.description = description
+    db.session.commit()
+
+    return equipment_schema.jsonify(equipment)
+
+
+@equipments.route("/inventory/delete/<id>", methods=["DELETE"])
+def inventory_del(id):
+    equipment = Inventory.query.get(id)
+    db.session.delete(equipment)
+    db.session.commit()
+
+    return equipment_schema.jsonify(equipment)
 
 
 @equipments.route("/lendings")
